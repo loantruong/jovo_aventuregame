19458c46d0d9f5aa76f18ecf38d545a0db01972d
loan <loancrea@gmail.com>
2019-02-06 23:09:17 +0100

    feat(intent): alexa color of door

--- src/app.js
+++ src/app.js
@@ -26,16 +26,26 @@ app.use(
 
 app.setHandler({
     LAUNCH() {
-        
+        let speech = "Préferez-vous prendre la porte bleu ou la porte rouge ?";
+        let reprompt = "Vous avez deux options, la bleu ou la rouge.";
+        this.ask(speech, reprompt);
     },
 
-    HelloWorldIntent() {
+    EnterDoorIntent() {
+        let speech = '';
+        let reprompt = '';
+        if(this.$inputs.color.value === 'bleu' || this.$inputs.color.value === 'rouge') {
+            speech = `Vous avez choisi la porte ${this.$inputs.color.value}.`;
+            this.ask(speech);
+        } else {
+            speech = 'Veuillez choisir la couleur de la porte, bleu ou rouge ?';
+            reprompt = 'La porte bleu ou la porte rouge.';
+            this.ask(speech, reprompt);
+        }
         
     },
-
-    MyNameIsIntent() {
-        
-    },
+    
+   
 });
 
 module.exports.app = app;
