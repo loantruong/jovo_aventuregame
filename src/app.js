1806b12f2a69e7956cc5dc87fd04941a3a8f5389
loan <loancrea@gmail.com>
2019-02-06 22:33:06 +0100

    init project

---
+++ src/app.js
@@ -0,0 +1,41 @@
+'use strict';
+
+// ------------------------------------------------------------------
+// APP INITIALIZATION
+// ------------------------------------------------------------------
+
+const { App } = require('jovo-framework');
+const { Alexa } = require('jovo-platform-alexa');
+const { GoogleAssistant } = require('jovo-platform-googleassistant');
+const { JovoDebugger } = require('jovo-plugin-debugger');
+const { FileDb } = require('jovo-db-filedb');
+
+const app = new App();
+
+app.use(
+    new Alexa(),
+    new GoogleAssistant(),
+    new JovoDebugger(),
+    new FileDb()
+);
+
+
+// ------------------------------------------------------------------
+// APP LOGIC
+// ------------------------------------------------------------------
+
+app.setHandler({
+    LAUNCH() {
+        
+    },
+
+    HelloWorldIntent() {
+        
+    },
+
+    MyNameIsIntent() {
+        
+    },
+});
+
+module.exports.app = app;
